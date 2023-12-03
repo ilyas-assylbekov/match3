2b035bd2a655b638c1f2ca78cd074a0cda49c9cc
illyForeverEarly <ilyas.asylbekov1305@gmail.com>
2023-12-03 21:11:58 +0600

    Implement value system to different tile variety

--- src/Board.lua
+++ src/Board.lua
@@ -13,10 +13,12 @@
 
 Board = Class{}
 
-function Board:init(x, y)
+function Board:init(x, y, level)
     self.x = x
     self.y = y
     self.matches = {}
+
+    self.level = level
 
     self:initializeTiles()
 end
@@ -32,7 +34,7 @@ function Board:initializeTiles()
         for tileX = 1, 8 do
             
             -- create a new tile at X,Y with a random color and variety
-            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random(6)))
+            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random( math.min( 6, self.level ) )))
         end
     end
 
@@ -240,7 +242,7 @@ function Board:getFallingTiles()
             if not tile then
 
                 -- new tile with random color and variety
-                local tile = Tile(x, y, math.random(18), math.random(6))
+                local tile = Tile(x, y, math.random(18), math.random( math.min( 6, self.level ) ))
                 tile.y = -32
                 self.tiles[y][x] = tile
 
